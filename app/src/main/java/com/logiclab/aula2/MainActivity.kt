c6173a13b22c80fd9e4f7f90983b71a91c40adfa
GuiVillas <villaszinho@gmail.com>
2026-03-06 08:53:33 -0300

    Ciclo de vida da activity

--- app/src/main/java/com/logiclab/aula2/MainActivity.kt
+++ app/src/main/java/com/logiclab/aula2/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.logiclab.aula2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "INFOTESTE"
+
+    /* ACTIVITY É CRIADA*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.i(TAG, "OnCreate")
+    }
+
+    /* ACTIVITY ESTÁ VISÍVEL AO USUÁRIO */
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "OnStart")
+    }
+
+    /* ACTIVITY ESTÁ PRONTA PARA USO */
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "OnResume")
+    }
+
+    /* PAUSA A ACTIVITY*/
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "OnPause")
+    }
+
+    /* SAI DA ACTIVITY */
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "OnStop")
+    }
+
+    /* ACTIVITY É FECHADA */
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "OnDestroy")
     }
 }
